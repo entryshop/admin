3c6addb72f0122b3d336a124e1893f75c8110b02
Alex <alex@parse.cn>
2024-08-13 21:46:28 +0800

    wip

--- resources/views/crud/fields/editor.blade.php
+++ resources/views/crud/fields/editor.blade.php
@@ -3,46 +3,7 @@
 @endphp
 
 <label>{{$renderable->label()}}</label>
-<div id="editor_{{$id}}"></div>
-<input name="{{$name}}" id="input_{{$id}}" type="hidden" value="{{$value}}"/>
 
-@once
-    @push('scripts')
-        <script src="{{admin()->asset('libs/tinymce/tinymce.min.js')}}"></script>
-        <script nonce="{{admin()->csp()}}">
-            const shadow_{{$id}} = document.getElementById('editor_{{$id}}').attachShadow({mode: 'open'});
-            const node_{{$id}} = document.createElement('textarea');
-            node_{{$id}}.append(`{!! $value !!}`);
-            shadow_{{$id}}.appendChild(node_{{$id}});
-            tinymce.init({
-                license_key: 'gpl',
-                target: node_{{$id}},
-                setup: function (editor) {
-                    editor.on('Change', function (e) {
-                        $('input[id=input_{{$id}}]').val(editor.getContent());
-                    });
-
-                    window.addEventListener('set_content_{{$id}}', function (e) {
-                        if (e.detail.content) {
-                            editor.setContent(e.detail.content);
-                        }
-                    });
-                },
-                height: "200",
-                plugins: 'fullscreen anchor code autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount',
-                toolbar: 'undo redo |blocks fontfamily fontsize |backcolor forecolor | bold italic underline strikethrough |code fullscreen link image media table mergetags | align lineheight | checklist numlist bullist indent outdent | emoticons | removeformat',
-                menubar: false,
-                images_upload_url: '/admin/upload?key=location',
-                custom_elements: "style"
-            });
-        </script>
-    @endpush
-
-    @push('styles')
-        <style nonce="{{admin()->csp()}}">
-            .tox-tinymce {
-                border-radius: 4px !important;
-            }
-        </style>
-    @endpush
-@endonce
+<x-admin::fields.tinymce
+    :$id :$name :$value
+/>
--- src/Http/Controllers/HomeController.php
+++ src/Http/Controllers/HomeController.php
@@ -1,12 +1,12 @@
 <?php
 
-
 namespace Entryshop\Admin\Http\Controllers;
 
 class HomeController
 {
     public function __invoke()
     {
+        admin()->title('Home');
         admin()->child('Welcome');
         return admin()->render();
     }
