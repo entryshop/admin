13485ef6cd413f9c65dca0b270a7dbd73c575b41
Alex <alex@parse.cn>
2024-08-20 16:27:50 +0800

    Refactor show.blade.php to include sections for before and after the header and body

--- resources/views/crud/show.blade.php
+++ resources/views/crud/show.blade.php
@@ -1,15 +1,31 @@
 @php
     $inline_buttons = $renderable->buttons('inline');
+    $section_before_header = $renderable->children('before_header');
+    $section_after_header = $renderable->children('after_header');
+    $section_before_body = $renderable->children('before_header');
+    $section_after_body = $renderable->children('section_after_body');
 @endphp
 
 <div class="card">
+    @if(!empty($section_before_header))
+        {!! render($section_before_header) !!}
+    @endif
+
     @if(count($inline_buttons))
         <div class="card-header">
             <x-admin::flex :wrap="false" :items="$inline_buttons" gap="2"
                            :params="['entity'=>$renderable->entity()]"/>
         </div>
     @endif
+
+    @if(!empty($section_after_header))
+        {!! render($section_after_header) !!}
+    @endif
+
     <div class="card-body">
+        @if(!empty($section_before_body))
+            {!! render($section_before_body) !!}
+        @endif
         <div {!! $renderable->wrapper() ?? 'class="d-flex flex-wrap gap-3"' !!} >
             @foreach($renderable->columns() as $column)
                 <div {!! $column->wrapper()?? $renderable->get('default-item-wrapper') !!}>
@@ -20,6 +36,9 @@
                 </div>
             @endforeach
         </div>
+        @if(!empty($section_after_body))
+            {!! render($section_after_body) !!}
+        @endif
     </div>
 </div>
 
