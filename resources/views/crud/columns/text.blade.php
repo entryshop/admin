e112774183d0ada9930413c2d5180c40ae585790
Alex <alex@parse.cn>
2024-09-24 12:32:05 +0800

    wip

--- resources/views/crud/columns/text.blade.php
+++ resources/views/crud/columns/text.blade.php
@@ -1,13 +1,27 @@
 @php
     $value = interpolate($value, $renderable->getContext());
+    if(!empty($href)) {
+        $link = true;
+        $href = interpolate($href, $renderable->getContext());
+        $target = $target??'_self';
+    } else {
+        $link = false;
+    }
+
 @endphp
 
-@if(empty($value))
-    {{to_string($value)}}
-@else
-    @if($escape??false)
-        {!! $value !!}
-    @else
-        {{ $value }}
-    @endif
+@if($link)
+    <a href="{{$href}}" target="{{$target}}">
+@endif
+        @if(empty($value))
+            {{to_string($value)}}
+        @else
+            @if($escape??false)
+                {!! $value !!}
+            @else
+                {{ $value }}
+            @endif
+        @endif
+@if($link)
+    </a>
 @endif
