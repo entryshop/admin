70afc2a906ed914b322433481150a43f51c8a95b
Alex <alex@parse.cn>
2024-10-01 16:19:07 +0800

    Fix missing value output in text column view

--- resources/views/crud/columns/text.blade.php
+++ resources/views/crud/columns/text.blade.php
@@ -14,6 +14,7 @@
     <a href="{{$href}}" target="{{$target}}">
 @endif
         @if(empty($value))
+            @dump($value)
             {{to_string($value)}}
         @else
             @if($escape??false)
