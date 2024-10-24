9db9d7f3b1abdc5f881310c0f269da20b49cfcb1
Alex <alex@parse.cn>
2024-10-24 19:58:43 +0800

    Update conditional checks to use isset() instead of !empty()

--- resources/views/widgets/info.blade.php
+++ resources/views/widgets/info.blade.php
@@ -17,7 +17,7 @@
             <div class="flex-grow-1">
                 <p class="fw-medium text-muted mb-0">{!! $title !!}</p>
             </div>
-            @if(!empty($right))
+            @if(isset($right))
                 <div class="flex-shrink-0">
                     {!! render($right) !!}
                 </div>
@@ -25,16 +25,16 @@
         </div>
         <div class="d-flex align-items-end justify-content-between mt-4">
             <div>
-                @if(!empty($value))
+                @if(isset($value))
                     <h4 class="fs-22 fw-semibold ff-secondary mb-0">
                         {!! render($value) !!}
                     </h4>
                 @endif
-                @if(!empty($link))
+                @if(isset($link))
                     <a href="{{$link}}" class="text-decoration-underline">{{$linkText??''}}</a>
                 @endif
             </div>
-            @if(!empty($icon))
+            @if(isset($icon))
                 <div class="avatar-sm flex-shrink-0">
                     <span class="avatar-title {{$color_icon_bg}} rounded fs-3 material-shadow">
                         <i class="{{$icon}} {{$color_icon_text}}"></i>
