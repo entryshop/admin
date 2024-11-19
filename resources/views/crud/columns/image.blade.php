80da7b718f83da5fd8d03d39083c309ff1ca73ad
Alex <alex@parse.cn>
2024-11-19 15:17:09 +0800

    Refactor image and file column views, add ImageColumn class

--- resources/views/crud/columns/image.blade.php
+++ resources/views/crud/columns/image.blade.php
@@ -1,1 +1,29 @@
-@include('admin::crud.columns.file')
+@php
+    if($stack??false)
+    {
+        $value = to_json($value);
+        $image_key ??= 'url';
+    }
+@endphp
+@if($stack??false)
+    <div>
+        @foreach($value as $_value)
+            <x-admin::columns.image
+                :src="$_value[$image_key]?:$defaultImageUrl"
+                :width="$width??null"
+                :height="$height??null"
+                :object_fit="$object_fit??'contain'"
+            />
+            @if(($limit??false) && $loop->iteration >= $limit)
+                @break
+            @endif
+        @endforeach
+    </div>
+@else
+    <x-admin::columns.image
+        :src="$value?:$defaultImageUrl"
+        :width="$width??null"
+        :height="$height??null"
+        :object_fit="$object_fit??'contain'"
+    />
+@endif
---
+++ src/Components/Crud/Columns/ImageColumn.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Entryshop\Admin\Components\Crud\Columns;
+
+use Entryshop\Admin\Components\Crud\CrudColumn;
+
+/**
+ * @method static defaultImageUrl($value) Default image url when no image is available
+ * @method self limit($value) Limit the number of images to display
+ */
+class ImageColumn extends CrudColumn
+{
+    protected $view_namespace = 'admin::crud.columns.';
+    protected $default_type = 'image';
+
+    /**
+     * Set max width and height
+     */
+    public function size($value)
+    {
+        $this->set('width', $value);
+        $this->set('height', $value);
+        return $this;
+    }
+
+    /**
+     * Display images in a stack
+     */
+    public function stack($value = true)
+    {
+        return $this->set('stack', $value);
+    }
+}
