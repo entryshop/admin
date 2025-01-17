33cdb34fba441c01a4ddce67c69f2ccce9dd7796
Nino_p <niucility@gmail.com>
2025-01-17 15:21:08 +0800

    add a description field for file field in form

--- resources/views/components/fields/field.blade.php
+++ resources/views/components/fields/field.blade.php
@@ -1,5 +1,8 @@
-@props(['name','label','id'])
-<label for="{{$id??''}}">{{$label??''}}</label>
+@props(['name','label','id','description'])
+<label for="{{$id??''}}">{{ $label??'' }}</label>
+@if($description ?? false)
+    <p>{!! $description??'dsads' !!}</p>
+@endif
 {{$slot}}
 <span class="text-danger error-message">
     @error($name)
--- resources/views/crud/fields/file.blade.php
+++ resources/views/crud/fields/file.blade.php
@@ -1,3 +1,3 @@
-<x-admin::fields.field :$name :id="$key" :label="$label??''">
+<x-admin::fields.field :$name :id="$key" :label="$label??''" :description="$description??null">
     <x-admin::fields.input :$name :value="old($name,$value??null)" :id="$key" type="file" class="form-control"/>
 </x-admin::fields.field>
