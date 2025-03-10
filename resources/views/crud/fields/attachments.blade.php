cfc98b64e20af0ca35c0263a7e73233269aa4fa9
imaxshuai <imaxshuai@gmail.com>
2025-03-10 15:04:51 +0800

    attachments field old value

--- resources/views/crud/fields/attachments.blade.php
+++ resources/views/crud/fields/attachments.blade.php
@@ -1,3 +1,3 @@
 <x-admin::fields.field :$name :id="$key" :label="$label??''">
-    <x-admin::fields.attachments :$name :value="$value??null" :id="$key"/>
+    <x-admin::fields.attachments :$name :value="old($name,$value??null)" :id="$key"/>
 </x-admin::fields.field>
